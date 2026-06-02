25e4f19608af74ab3806949424b55e40352f2c50
tarikulislam <tarikulislam4145@gmail.com>
2026-06-02 21:45:32 +0530

    Test

---
+++ app.js
@@ -0,0 +1,104 @@
+const socket = io(
+"http://20.187.121.94:3000/" , {
+    transports: ["websocket"]
+  }
+);
+
+const localVideo = document.getElementById("localVideo");
+const remoteVideo = document.getElementById("remoteVideo");
+
+const roomInput = document.getElementById("roomId");
+const joinBtn = document.getElementById("joinBtn");
+
+let localStream;
+let peerConnection;
+let roomId;
+
+const servers = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+};
+
+async function initMedia() {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  });
+
+  localVideo.srcObject = localStream;
+}
+
+async function createPeerConnection() {
+  peerConnection = new RTCPeerConnection(servers);
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    remoteVideo.srcObject = event.streams[0];
+  };
+
+  peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      socket.emit("ice-candidate", {
+        roomId,
+        candidate: event.candidate,
+      });
+    }
+  };
+}
+
+joinBtn.addEventListener("click", async () => {
+  roomId = roomInput.value;
+
+  if (!roomId) {
+    alert("Enter room id");
+    return;
+  }
+
+  await createPeerConnection();
+
+  socket.emit("join-room", roomId);
+});
+
+socket.on("user-joined", async () => {
+  const offer = await peerConnection.createOffer();
+
+  await peerConnection.setLocalDescription(offer);
+
+  socket.emit("offer", {
+    roomId,
+    offer,
+  });
+});
+
+socket.on("offer", async (offer) => {
+  await peerConnection.setRemoteDescription(offer);
+
+  const answer = await peerConnection.createAnswer();
+
+  await peerConnection.setLocalDescription(answer);
+
+  socket.emit("answer", {
+    roomId,
+    answer,
+  });
+});
+
+socket.on("answer", async (answer) => {
+  await peerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice-candidate", async (candidate) => {
+  try {
+    await peerConnection.addIceCandidate(candidate);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+initMedia();
