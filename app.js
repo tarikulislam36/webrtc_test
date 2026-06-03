56f6634b5855a51660da0c5360a2af521f253ca3
TARIKUL ISLAM <105806749+tarikulislam36@users.noreply.github.com>
2026-06-03 18:39:17 +0530

    update stun server

--- app.js
+++ app.js
@@ -17,9 +17,23 @@ let roomId;
 const servers = {
   iceServers: [
     {
-      urls: "stun:stun.l.google.com:19302",
+      urls: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun.cloudflare.com:3478"
+      ]
     },
-  ],
+    {
+      urls: [
+        "turn:openrelay.metered.ca:80",
+        "turn:openrelay.metered.ca:443",
+        "turn:openrelay.metered.ca:443?transport=tcp"
+      ],
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
+  ]
 };
 
 async function initMedia() {
