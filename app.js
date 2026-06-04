ee7949ea0ae9e09318b51f38f07e2fa8f3e48390
TARIKUL ISLAM <105806749+tarikulislam36@users.noreply.github.com>
2026-06-04 23:55:37 +0530

    Remove redundant STUN server URL

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const servers = {
     {
       urls: [
         "stun:stun.l.google.com:19302",
-        "stun:stun1.l.google.com:19302",
         "stun:stun2.l.google.com:19302",
         "stun:stun.cloudflare.com:3478"
       ]
